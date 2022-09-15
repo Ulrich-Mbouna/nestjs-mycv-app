991837c14d044c7342adf7b3b640240094663f93
Mbouna Ulrich <msuslegrand@gmail.com>
2022-09-15 02:35:08 +0100

    Mise en place de la création des reports: - Prise en charge des rélations en tre entité du système - Prise ne charge des contraintes et bonne facon de faire les choses Work from Home 😁

---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,34 @@
+import {Expose, Transform} from "class-transformer";
+import {User} from "../../users/user.entity";
+
+export class ReportDto {
+    @Expose()
+    id: number;
+
+    @Transform(({ obj }) => (
+        {
+            id: obj.user.id,
+            email: obj.user.email,
+        }))
+    @Expose()
+    user: User;
+
+    @Transform(({ obj }) => obj.user.id)
+    @Expose()
+    userId: number
+
+    @Expose()
+    price: number;
+    @Expose()
+    year: number;
+    @Expose()
+    lng: number;
+    @Expose()
+    lat: number;
+    @Expose()
+    make: string;
+    @Expose()
+    model: string;
+    @Expose()
+    mileage: string;
+}
